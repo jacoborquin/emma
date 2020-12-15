83fb5a948b0ee02030b28a9692d3e597c6360442
Jacob L. Orquin <jalo@mgmt.au.dk>
2020-12-15 09:27:15 +0100

    icc vals

--- scripts/intercoder_reliability.R
+++ scripts/intercoder_reliability.R
@@ -41,12 +41,6 @@ coder4 = read_excel(
 # join coder realiability data for old data
 ICCdata = left_join(coder1, coder2, by = "Paper")
 
-# join coder reliability for recoded ES data (revision round 2)
-coder3 = melt(coder3, id.vars = c(2,3,10,11), measure.vars = c(4:7))
-coder4 = melt(coder4, id.vars = c(2,3,10,11), measure.vars = c(4:7))
-ESdata = data.table(merge(coder3, coder4, by = c("Study", "IV", "Research.Domain", "Alt.att", "variable"), all.x = T))
-ESdata = ESdata[is.na(ESdata$value.x) == F & is.na(ESdata$value.y) == F]
-
 # kappa for categorical variables
 DV_kappa = kappa2(ICCdata[,c("DV.x","DV.y")], "unweighted")
 IV_kappa = kappa2(ICCdata[,c("IV.x","IV.y")], "unweighted")
@@ -54,7 +48,7 @@ ET_kappa = kappa2(ICCdata[,c("EyeTracker.x","EyeTracker.y")], "unweighted")
 domain_kappa = kappa2(ICCdata[,c("Domain.x","Domain.y")], "unweighted")
 
 # ICC for continuous variables
-ES_ICC = icc(ESdata[,c("value.x","value.y")], model="oneway", type="agreement")
+ES_ICC = icc(ICCdata[,c("ES.x","ES.y")], model="oneway", type="agreement")
 N_ICC = icc(ICCdata[,c("N.x","N.y")], model="oneway", type="agreement")
 
 # paste summary of inter coder reliability for manuscript
@@ -67,3 +61,14 @@ result <- paste0(
 	" independent variable, $\\kappa = ", round(IV_kappa$value, 3),"$"
 )
 cat(result, file = file.path(tablesDir, "intercoder_reliability.tex"))
+
+# join coder reliability for recoded ES data (revision round 2)
+coder3 = melt(coder3, id.vars = c(2,3,10,11), measure.vars = c(4:7))
+coder4 = melt(coder4, id.vars = c(2,3,10,11), measure.vars = c(4:7))
+ESdata = data.table(merge(coder3, coder4, by = c("Study", "IV", "Research.Domain", "Alt.att", "variable"), all.x = T))
+ESdata = ESdata[is.na(ESdata$value.x) == F & is.na(ESdata$value.y) == F]
+ES_ICC2 = icc(ESdata[,c("value.x","value.y")], model="oneway", type="agreement")
+
+result <- paste0("$\\textrm{ICC} = ", round(ES_ICC2$value, 3),"$,")
+cat(result, file = file.path(tablesDir, "intercoder_reliability2.tex"))
+
